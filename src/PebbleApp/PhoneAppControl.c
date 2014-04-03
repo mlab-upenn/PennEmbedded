01ab2c5fd72a813d3aaf09b0d964f1239f18b1da
Aayush Sharma <aayushsharma@seas1452.wireless-pennnet.upenn.edu>
2014-04-03 15:10:27 -0400

    Updated Pebble WatchApp

--- src/PebbleApp/PhoneAppControl.c
+++ src/PebbleApp/PhoneAppControl.c
@@ -1,70 +1,99 @@
+/*****************************************
+ To understand the various parts of this code, you must understand the anatomy of a Pebble WatchApp. 
+ Go through this page to undersatnd them: https://developer.getpebble.com/2/guides/anatomy-of-pebble-application.html
+ *****************************************/
+
+
 #include <pebble.h>
 #include <stdlib.h>
-/*
-enum {
-	CMD_KEY = 0x01, // TUPLE_INTEGER
-};
 
-enum {
-	CMD_UP = 0x01, CMD_DOWN = 0x02, CMD_SELECT = 0x00
-};
-*/
 
 #define MY_UUID {0xF0, 0x73, 0x4B, 0x0E, 0x18, 0x9E, 0x43, 0x99, 0xB2, 0xA0, 0xF1, 0x61, 0x6A, 0xDD, 0xE5, 0x54}
-
 
 
 static Window *window;
 static TextLayer *text_layer;
 
+/*
+ This function is called when an outgoing message is delivered to the App on the Paired Device
+ */
 static void out_sent_handler(DictionaryIterator *sent, void *context) {
     // outgoing message was delivered
-    text_layer_set_text(text_layer, "Sent");
+    text_layer_set_text(text_layer, "Sent");//Function to display content on the WatchApp
 }
 
-
+/*
+ This function is called when an outgoing message fails to get delivered to the App on the Paired Device
+ */
 static void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, void *context) {
-    // outgoing message failed
     text_layer_set_text(text_layer, "Failed");
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "App Message Sync Error: %d", reason);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "App Message Sync Error: %d", reason);//This can be used to display logs on your machine while the WatchApp runs  
 }
 
-
+/*
+ This function is called when there is an incoming message from the App on the Paired Device
+ */
 static void in_received_handler(DictionaryIterator *received, void *context) {
-    // incoming message received
+ 
 }
 
-
+/*
+ This function is called when an incoming message from the App on the Paired Device is dropped
+ */
 static void in_dropped_handler(AppMessageResult reason, void *context) {
-    // incoming message dropped
+    // handling incoming message drop event
 }
 
+/*
+ This function receives the data to be sent from the click handlers and sends it over the Bluetooth interface.
+ Follow this link for details: https://developer.getpebble.com/2/guides/app-phone-communication.html
+ */
 static void send_cmd(int key) {
     DictionaryIterator *iter;
     app_message_outbox_begin(&iter);
     Tuplet value = TupletInteger(key,0);
     dict_write_tuplet(iter, &value);
-    //APP_LOG(APP_LOG_LEVEL_DEBUG, "Key: %d", CMD_KEY);
-    app_message_outbox_send();
-    text_layer_set_text(text_layer, "Sending");
     
+    app_message_outbox_send();
+    
+    if (key == 0)
+        text_layer_set_text(text_layer, "Go Forward");
+    else if (key == 1)
+        text_layer_set_text(text_layer, "Go  Left");
+    else
+        text_layer_set_text(text_layer, "Go Right");
 }
 
+
+/*
+ This function is called when the select (middle) button is pressed on the Pebble.
+ Send appropriate command. '0' in this case.
+ */
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
     send_cmd(0);
     //text_layer_set_text(text_layer, c);
 }
 
+
+/*
+ This function is called when the up button is pressed on the Pebble.
+ Press appropriate message. '1' in this case.
+ */
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
     send_cmd(1);
-    
-    //text_layer_set_text(text_layer, c);
 }
 
+
+
+/*
+This function is called when the down button is pressed on the Pebble.
+Press appropriate message. '2' in this case.
+ */
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
     send_cmd(2);
-    //text_layer_set_text(text_layer, c);
 }
+
+
 
 static void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
@@ -106,9 +135,6 @@ static void init(void) {
     
     
   window_stack_push(window, animated);
-    
-    
-
 }
 
 static void deinit(void) {
